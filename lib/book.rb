b8c5328d4a4e8e3d296b9e2dd04799e00bf7f314
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-28 11:03:47 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,11 @@
 class Book
   attr_accessor :author, :page_count, 
-  attr_reader :title
+  attr_reader :title, :genre
 GENRES = []
   def initialize(title)
     @title = title
   end
+  
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
